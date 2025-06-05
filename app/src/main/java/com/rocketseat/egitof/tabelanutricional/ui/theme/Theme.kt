fb46d76756edc75e47c787cbe5f4522dbfcc542b
Egito Filho <egitofilho4@gmail.com>
2025-06-05 18:46:34 -0300

    06_25_COMPLEMENTANDO_A_TEMATIZACAO_COM_AS_COMPOSICOES_LOCAIS

--- app/src/main/java/com/rocketseat/egitof/tabelanutricional/ui/theme/Theme.kt
+++ app/src/main/java/com/rocketseat/egitof/tabelanutricional/ui/theme/Theme.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 private val LightColorScheme = lightColorScheme(
     primary = Primary,
@@ -34,11 +36,13 @@ private val DarkColorScheme = darkColorScheme(
 
 @Composable
 fun TabelaNutricionalTheme(
+    sizing: Sizing = Sizing(),
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val sizing = if (darkTheme) Sizing(sm = 56.dp) else sizing
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
@@ -52,6 +56,18 @@ fun TabelaNutricionalTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = {
+            CompositionLocalProvider(
+                LocalSizing provides(sizing)
+            ) {
+                content()
+            }
+        }
     )
+}
+
+object TabelaNutricionalTheme {
+    val sizing: Sizing
+        @Composable
+        get() = LocalSizing.current
 }
