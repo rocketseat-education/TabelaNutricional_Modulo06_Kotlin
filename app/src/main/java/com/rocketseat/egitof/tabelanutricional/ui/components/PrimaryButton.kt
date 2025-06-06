2038d10773242e4718c89793faad735bbd452936
Egito Filho <egitofilho4@gmail.com>
2025-06-05 21:28:12 -0300

    06_27_CONSTRUINDO_OS_COMPONENTES_VISUAIS_BASICOS_PARTE_02

---
+++ app/src/main/java/com/rocketseat/egitof/tabelanutricional/ui/components/PrimaryButton.kt
@@ -0,0 +1,47 @@
+package com.rocketseat.egitof.tabelanutricional.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.rocketseat.egitof.tabelanutricional.ui.theme.Primary
+import com.rocketseat.egitof.tabelanutricional.ui.theme.TabelaNutricionalTheme
+import com.rocketseat.egitof.tabelanutricional.ui.theme.Typography
+
+@Composable
+fun PrimaryButton(modifier: Modifier = Modifier, text: String, onClick: () -> Unit) {
+    ElevatedButton(
+        modifier = modifier,
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor = Primary,
+        ),
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = 4.dp
+        ),
+        shape = RoundedCornerShape(size = 16.dp),
+        onClick = onClick
+    ) {
+        Text(
+            text = text,
+            style = Typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PrimaryButtonPreview() {
+    TabelaNutricionalTheme {
+        PrimaryButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Mais detalhes"
+        ) { }
+    }
+}
