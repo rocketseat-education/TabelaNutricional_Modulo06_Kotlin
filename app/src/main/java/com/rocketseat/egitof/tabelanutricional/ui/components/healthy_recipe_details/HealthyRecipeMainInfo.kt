e01274509bbc8a54ee7d7341060137afbc4be6df
Egito Filho <egitofilho4@gmail.com>
2025-06-15 14:19:35 -0300

    06_33_CONSTRUINDO_COMPONENTES_DA_TELA_DE_DETALHES_DA_RECEITA_PARTE_01

---
+++ app/src/main/java/com/rocketseat/egitof/tabelanutricional/ui/components/healthy_recipe_details/HealthyRecipeMainInfo.kt
@@ -0,0 +1,81 @@
+package com.rocketseat.egitof.tabelanutricional.ui.components.healthy_recipe_details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.rocketseat.egitof.tabelanutricional.R
+import com.rocketseat.egitof.tabelanutricional.ui.theme.TabelaNutricionalTheme
+import com.rocketseat.egitof.tabelanutricional.ui.theme.TabelaNutricionalTheme.sizing
+import com.rocketseat.egitof.tabelanutricional.ui.theme.Typography
+
+@Composable
+fun HealthyRecipeMainInfo(
+    modifier: Modifier = Modifier,
+    recipeName: String,
+    calories: Float,
+    totalPortionInGrams: Int
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = recipeName,
+            style = Typography.headlineLarge,
+        )
+        Spacer(modifier = Modifier.height(sizing.lg))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(sizing.sm),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.energia),
+                    style = Typography.headlineSmall
+                )
+                Text(
+                    text = stringResource(R.string.valor_kcal, calories),
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.spacedBy(sizing.sm),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.porcao_total),
+                    style = Typography.headlineSmall
+                )
+                Text(
+                    text = stringResource(R.string.valor_g, totalPortionInGrams),
+
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HealthyRecipeMainInfoPreview() {
+    TabelaNutricionalTheme {
+        HealthyRecipeMainInfo(
+            modifier = Modifier.padding(vertical = sizing.md),
+            recipeName = "Salada variada",
+            calories = 221.15f,
+            totalPortionInGrams = 200
+        )
+    }
+}
